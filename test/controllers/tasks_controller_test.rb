8aaba06b11e53a578c24a89459d3b33605469443
Jorge Esmeral <jorgeesmeral@hotmail.com>
2015-03-06 03:08:45 +0000

    Correccion de test unitarios y funcionales‏

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,17 +6,20 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
+    sign_in users(:one)
     get :index
     assert_response :success
     assert_not_nil assigns(:tasks)
   end
 
   test "should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
 
   test "should create task" do
+    sign_in users(:one)
     assert_difference('Task.count') do
       post :create, task: { name: @task.name, status: @task.status, user_id: @task.user_id }
     end
@@ -25,21 +28,25 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should show task" do
+    sign_in users(:one)
     get :show, id: @task
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get :edit, id: @task
     assert_response :success
   end
 
   test "should update task" do
+    sign_in users(:one)
     patch :update, id: @task, task: { name: @task.name, status: @task.status, user_id: @task.user_id }
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should destroy task" do
+    sign_in users(:one)
     assert_difference('Task.count', -1) do
       delete :destroy, id: @task
     end
